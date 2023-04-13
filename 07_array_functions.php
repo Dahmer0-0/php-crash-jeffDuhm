e6eaa0d6602e10e5aac0a59ab66ccfe0f0222807
Dahmer0-0 <113373992+Dahmer0-0@users.noreply.github.com>
2023-04-12 23:00:30 -0500

    Update 07_array_functions.php

--- 07_array_functions.php
+++ 07_array_functions.php
@@ -8,24 +8,24 @@
 
 $fruits = ['apple', 'banana', 'orange'];
 
-// Get array length
+// Obtener la longitud de la matriz
 echo count($fruits);
 
 // Search array
 echo in_array('banana', $fruits);
 
-// Add to an array
+// Agregar a una matriz
 $fruits[] = 'grape';
 array_push($fruits, 'mango', 'raspberry');
-array_unshift($fruits, 'kiwi'); // Adds to the beginning
+array_unshift($fruits, 'kiwi'); // Agrega al principio
 
-// Remove from array
+// Eliminar de la matriz
 array_pop($fruits); // Removes last
 array_shift($fruits); // Removes first
-// Remove specific element
+// Eliminar elemento específico
 unset($fruits[2]);
 
-// Split into chunks of 2
+// Dividir en trozos de 2 - split
 $chunkedArray = array_chunk($fruits, 2);
 var_dump($chunkedArray);
 
@@ -34,7 +34,7 @@ $arr1 = [1, 2, 3];
 $arr2 = [4, 5, 6];
 $arr3 = array_merge($arr1, $arr2);
 var_dump($arr3);
-$arr4 = [...$arr1, ...$arr2]; // Use Spread
+$arr4 = [...$arr1, ...$arr2]; // Usar propagación
 var_dump($arr4);
 
 // Combine arrays (Keys & values)
@@ -60,7 +60,7 @@ $newNumbers = array_map(function ($number) {
 // Filter array
 $lessThan10 = array_filter($numbers, fn($number) => $number < 10);
 
-// Array Reduce
-// "carry" holds the return value of the previous iteration
+// Array Reduce - Reducir matriz
+// "carry" contiene el valor de retorno de la iteración anterior
 $sum = array_reduce($numbers, fn($carry, $number) => $carry + $number);
 var_dump($sum);
